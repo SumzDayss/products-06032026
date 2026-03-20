c385512345b09ebfe6b8bc280ca6b638c6239757
name <you@example.com>
2026-03-20 13:02:59 +0200

    Added a route to /info, which contains a paragraph

---
+++ resources/views/info.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quisquam eum voluptates eaque cum iste rem ducimus, eveniet eligendi perspiciatis magnam. Quod reiciendis dignissimos pariatur.</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-//Pievienojiet aplikācijai jaunu maršrutu, kas saņem lietotāja ceļu '/about' un apstrādā to ar anonīmu funkciju izvadot 'h1' elementu ar tekstu "About this page" bez skata (view).
 Route::get('/about',function(){
     return "<h1>About this page</h1>";
+});
+Route::get('/info',function(){
+    return view('info');
 });
