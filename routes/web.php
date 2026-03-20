7c9a16b494d5ab3ca4c87de6232163e59c56900f
name <you@example.com>
2026-03-20 13:00:20 +0200

    Added a route to /about that shows an h1 text

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//Pievienojiet aplikācijai jaunu maršrutu, kas saņem lietotāja ceļu '/about' un apstrādā to ar anonīmu funkciju izvadot 'h1' elementu ar tekstu "About this page" bez skata (view).
+Route::get('/about',function(){
+    return "<h1>About this page</h1>";
+});
